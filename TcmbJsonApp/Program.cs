7e9bba0f6ffdeda9955f4f12c97395339110be16
Mehmet Can Gündüz <gunduzm410@gmail.com>
2023-11-10 16:01:37 +0300

    Proje dosyası ekle.

---
+++ TcmbJsonApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+
+namespace CurrencyApplicationJson
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string json = GetJson();
+
+            SaveJson(json);
+
+
+        }
+
+        static string GetJson()
+        {
+            DateTime today = DateTime.Now;
+            DateTime lastYear = today.AddYears(-1);
+            string formattedDate = lastYear.ToString("dd-MM-yyyy");
+            WebRequest request = WebRequest.Create($"https://evds2.tcmb.gov.tr/service/evds/series=TP.DK.USD.S.YTL&startDate={formattedDate}&endDate={formattedDate}&type=json&key=N5ZetAfwcG&aggregationTypes=avg&formulas=&frequency=");
+
+
+            request.GetResponseAsync();
+
+
+            StreamReader reader = new StreamReader(request.GetResponse().GetResponseStream());
+            string json = reader.ReadToEnd();
+            reader.Close();
+
+            return json;
+        }
+
+        static void SaveJson(string json)
+        {
+
+            string fileName = "dolar-kuru.json";
+            StreamWriter writer = new StreamWriter(fileName);
+            writer.Write(json);
+            writer.Close();
+            Console.WriteLine(json);
+        }
+
+
+    }
+}
